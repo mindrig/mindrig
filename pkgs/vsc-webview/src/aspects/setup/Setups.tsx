35ae80580299a1af315b8abc5122ca8531c306ff
Sasha Koss <koss@nocorp.me>
2025-11-27 06:39:14 +0800

    Theme setup layout and add model button

--- pkgs/vsc-webview/src/aspects/setup/Setups.tsx
+++ pkgs/vsc-webview/src/aspects/setup/Setups.tsx
@@ -1,6 +1,8 @@
 import { buildSetup, buildSetupsModelRefs, Setup } from "@wrkspc/core/setup";
 import { Button } from "@wrkspc/ds";
+import iconRegularPlus from "@wrkspc/icons/svg/regular/plus.js";
 import { Field, State } from "enso";
+import { LayoutSection } from "../layout/Section";
 import { useModelsMap } from "../model/MapContext";
 import { SetupComponent } from "./Setup";
 import { SetupsAppState } from "./setupsAppState";
@@ -19,41 +21,47 @@ export function Setups(props: Setups.Props) {
   const soloSetup = setupsField.useCompute((setups) => setups.length === 1, []);
 
   return (
-    <div className="space-y-2">
-      <div className="flex items-center justify-between">
-        <div className="flex items-center gap-2">
-          <h4 className="text-sm font-semibold">
-            {soloSetup ? "Model" : "Models"}
-          </h4>
+    <LayoutSection bordered>
+      <div className="space-y-2">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-2">
+            <h4 className="text-sm font-semibold">
+              {soloSetup ? "Model" : "Models"}
+            </h4>
+          </div>
+
+          <Button
+            type="button"
+            color="secondary"
+            style="transparent"
+            size="xsmall"
+            icon={iconRegularPlus}
+            onClick={() => {
+              const usedModelRefs = buildSetupsModelRefs(setupsField.value);
+              setupsField.push(
+                buildSetup({
+                  modelsMap: modelsPayload?.map,
+                  usedModelRefs,
+                }),
+              );
+            }}
+          >
+            Add model
+          </Button>
         </div>
 
-        <Button
-          type="button"
-          onClick={() => {
-            const usedModelRefs = buildSetupsModelRefs(setupsField.value);
-            setupsField.push(
-              buildSetup({
-                modelsMap: modelsPayload?.map,
-                usedModelRefs,
-              }),
-            );
-          }}
-        >
-          {soloSetup ? "Multi model" : "Add model"}
-        </Button>
+        <div className="space-y-3">
+          {setupsField.map((setupField, index) => (
+            <SetupComponent
+              key={setupField.key}
+              setupField={setupField}
+              solo={soloSetup}
+              expandedIndexState={setupsAppState.$.expandedIndex}
+              index={index}
+            />
+          ))}
+        </div>
       </div>
-
-      <div className="space-y-3">
-        {setupsField.map((setupField, index) => (
-          <SetupComponent
-            key={setupField.key}
-            setupField={setupField}
-            solo={soloSetup}
-            expandedIndexState={setupsAppState.$.expandedIndex}
-            index={index}
-          />
-        ))}
-      </div>
-    </div>
+    </LayoutSection>
   );
 }
