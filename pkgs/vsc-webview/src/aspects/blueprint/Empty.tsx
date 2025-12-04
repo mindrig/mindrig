3c9dea666c4312fbac36f8724aa4d0a8df69120f
Sasha Koss <koss@nocorp.me>
2025-12-04 18:51:07 +0800

    Style no prompt block

--- pkgs/vsc-webview/src/aspects/blueprint/Empty.tsx
+++ pkgs/vsc-webview/src/aspects/blueprint/Empty.tsx
@@ -1,3 +1,6 @@
+import { Icon, textCn } from "@wrkspc/ds";
+import iconRegularHandPointer from "@wrkspc/icons/svg/regular/hand-pointer.js";
+import { Block } from "@wrkspc/ui";
 import { useEffect } from "react";
 import { log } from "smollog";
 
@@ -6,5 +9,19 @@ export function BlueprintEmpty() {
     log.debug("No prompt selected");
   }, []);
 
-  return <div>No prompt selected.</div>;
+  return (
+    <Block dir="y" pad={[false, "medium", "medium"]}>
+      <Block dir="y" size="small" align pad={["xlarge", "large"]} border>
+        <Icon id={iconRegularHandPointer} size="xlarge" color="support" />
+
+        <h2 className={textCn({ role: "label", size: "large" })}>
+          No prompt selected
+        </h2>
+
+        <p className={textCn({ color: "support" })}>
+          Focus a prompt in the source code to test it.
+        </p>
+      </Block>
+    </Block>
+  );
 }
