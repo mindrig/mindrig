ae6dda7d0e06f4686634b6e4b3918fca3f240c74
Sasha Koss <koss@nocorp.me>
2025-12-12 05:27:12 +0000

    Hide drafts section when there's none

--- pkgs/vsc-webview/src/aspects/playground/PromptSelector.tsx
+++ pkgs/vsc-webview/src/aspects/playground/PromptSelector.tsx
@@ -63,6 +63,7 @@ export function PlaygroundPromptSelector(
               };
             },
           ),
+          flatten: !hasDrafts,
         },
       ]}
       placeholder={prompts.size ? "Select prompt" : "No prompts"}
