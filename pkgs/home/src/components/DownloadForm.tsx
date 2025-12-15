59e93a464735ff129632dd0a7b4e8ac526bce37e
Sasha Koss <koss@nocorp.me>
2025-12-15 07:26:39 +0000

    Simple download page

--- pkgs/home/src/components/DownloadForm.tsx
+++ pkgs/home/src/components/DownloadForm.tsx
@@ -1,4 +1,5 @@
-import { Button } from "@wrkspc/ui";
+import { textCn } from "@wrkspc/theme";
+import { Block, Button } from "@wrkspc/ui";
 
 export interface DownloadFormProps {
   inverse?: boolean;
@@ -6,10 +7,40 @@ export interface DownloadFormProps {
 
 export function DownloadForm(props: DownloadFormProps) {
   return (
-    <div className="max-w-7xl w-full flex flex-col gap-4 items-center">
-      <Button color="cta" size="large" href="#">
-        Download at VS Code Marketplace
-      </Button>
-    </div>
+    <Block dir="y" align>
+      <h2
+        className={textCn({
+          role: "label",
+          color: "support",
+          size: "large",
+        })}
+      >
+        Install Extension For:
+      </h2>
+
+      <Block justify align>
+        <Button
+          color="cta"
+          size="large"
+          href="https://marketplace.visualstudio.com/items?itemName=mindrig.vscode"
+        >
+          Visual Studio Code
+        </Button>
+
+        <span
+          className={textCn({ role: "label", size: "small", color: "detail" })}
+        >
+          Or
+        </span>
+
+        <Button
+          color="cta"
+          size="large"
+          href="https://open-vsx.org/extension/mindrig/vscode"
+        >
+          Cursor, Antigravity, etc.
+        </Button>
+      </Block>
+    </Block>
   );
 }
