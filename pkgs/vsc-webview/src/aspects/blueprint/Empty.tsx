6055213408744a0d3beaef69069a63cf1c374a59
Sasha Koss <koss@nocorp.me>
2025-12-16 03:45:52 +0000

    Add supported code tip

--- pkgs/vsc-webview/src/aspects/blueprint/Empty.tsx
+++ pkgs/vsc-webview/src/aspects/blueprint/Empty.tsx
@@ -1,8 +1,9 @@
 import { EditorFile } from "@wrkspc/core/editor";
-import { textCn } from "@wrkspc/ds";
+import { Icon, textCn } from "@wrkspc/ds";
 import iconRegularFileBan from "@wrkspc/icons/svg/regular/file-ban.js";
 import iconRegularHandPointer from "@wrkspc/icons/svg/regular/hand-pointer.js";
-import { Button } from "@wrkspc/ui";
+import iconRegularLightbulb from "@wrkspc/icons/svg/regular/lightbulb.js";
+import { Block, Button } from "@wrkspc/ui";
 import { State } from "enso";
 import { useMessages } from "../message/Context";
 import { PageEmpty } from "../page/Empty";
@@ -45,6 +46,52 @@ export function BlueprintEmpty(props: BlueprintEmpty.Props) {
 
           <PlaygroundPromptSelector />
         </>
+      }
+      extra={
+        !isUnknown && (
+          <Block
+            dir="y"
+            size="xsmall"
+            align
+            border="top"
+            pad={{ top: "large" }}
+          >
+            <Block size="xsmall" align>
+              <Icon id={iconRegularLightbulb} size="small" color="support" />
+              <h3
+                className={textCn({
+                  bold: true,
+                  size: "small",
+                  color: "detail",
+                  align: "center",
+                  balance: true,
+                })}
+              >
+                Quick Tip
+              </h3>
+            </Block>
+
+            <p
+              className={textCn({
+                size: "small",
+                color: "detail",
+                align: "center",
+                balance: true,
+              })}
+            >
+              To make Mind Rig detect prompts in your code, assign a string to a
+              variable with <code>prompt</code> in name, or put an annotation
+              comment <code>// @prompt</code> or <code>/* @prompt */</code> in
+              front of a line or string.{" "}
+              <a
+                href="https://mindrig.ai/docs/guides/prompts-detection/"
+                className="text-link hover:text-link-hover"
+              >
+                Learn more
+              </a>
+            </p>
+          </Block>
+        )
       }
     />
   );
--- pkgs/vsc-webview/src/aspects/page/Empty.tsx
+++ pkgs/vsc-webview/src/aspects/page/Empty.tsx
@@ -9,45 +9,50 @@ export namespace PageEmpty {
     description: string;
     actions?: React.ReactNode | undefined;
     notices?: React.ReactNode | undefined;
+    extra?: React.ReactNode | undefined;
   }
 }
 
 export function PageEmpty(props: PageEmpty.Props) {
-  const { icon, label, description, actions, notices } = props;
+  const { icon, label, description, actions, notices, extra } = props;
 
   return (
     <Block dir="y" gap={false}>
       {notices}
 
       <Block dir="y" pad={[false, "medium", "medium"]}>
-        <Block dir="y" size="small" align pad={["xlarge", "large"]} border>
-          <Icon id={icon} size="xlarge" color="support" />
+        <Block dir="y" size="large" align pad={["xlarge", "large"]} border>
+          <Block dir="y" size="small" align>
+            <Icon id={icon} size="xlarge" color="support" />
 
-          <Block dir="y" size="xsmall">
-            <h2
-              className={textCn({
-                role: "label",
-                align: "center",
-                size: "large",
-              })}
-            >
-              {label}
-            </h2>
+            <Block dir="y" size="xsmall">
+              <h2
+                className={textCn({
+                  role: "label",
+                  align: "center",
+                  size: "large",
+                })}
+              >
+                {label}
+              </h2>
 
-            <p
-              className={textCn({
-                color: "support",
-                align: "center",
-                className: "max-w-sm text-balance",
-              })}
-            >
-              {description}
-            </p>
+              <p
+                className={textCn({
+                  color: "support",
+                  align: "center",
+                  className: "max-w-sm text-balance",
+                })}
+              >
+                {description}
+              </p>
+            </Block>
+
+            <Block size="small" align>
+              {actions}
+            </Block>
           </Block>
 
-          <Block size="small" align>
-            {actions}
-          </Block>
+          {extra}
         </Block>
       </Block>
     </Block>
